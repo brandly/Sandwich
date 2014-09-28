c5fb27b1f78ae32bebdc6a55ba953770cde697dd
brandly <brandly13@gmail.com>
2014-09-28 05:04:29 -0400

    create RedditPost and DetailTableViewCell

--- Sandwich/Reddit.swift
+++ Sandwich/Reddit.swift
@@ -10,7 +10,7 @@ import Foundation
 import Alamofire
 
 class Reddit {
-    class func getPosts(subreddit: String, done: (([NSDictionary]) -> Void)) {
+    class func getPosts(subreddit: String, done: (([RedditPost]) -> Void)) {
         var url = "http://www.reddit.com/r/" + subreddit + "/.json"
         Alamofire.request(.GET, url)
             .responseJSON { (_, _, responseData, error) in
@@ -21,13 +21,42 @@ class Reddit {
                     var childrenData: AnyObject? = allData?["children"]
                     
                     if var children = childrenData as? [NSDictionary] {
-                        func parsePost(post: NSDictionary) -> NSDictionary {
-                            return post["data"] as NSDictionary
+                        func parsePost(post: NSDictionary) -> RedditPost {
+                            let postDictionary = post["data"] as NSDictionary
+                            return RedditPost(data: postDictionary)
                         }
-                        var postData: [NSDictionary] = children.map(parsePost)
+                        var postData: [RedditPost] = children.map(parsePost)
                         done(postData)
                     }
                 }
         }
+    }
+}
+
+class RedditPost {
+    var author: String
+    var domain: String
+    var is_self: Bool
+    var num_comments: UInt
+    var over_18: Bool
+    var score: Int
+    var subreddit: String
+    var subreddit_id: String
+    var thumbnail: String
+    var title: String
+    var url: String
+    
+    init(data: NSDictionary) {
+        self.author = data["author"] as String
+        self.domain = data["domain"] as String
+        self.is_self = data["is_self"] as Bool
+        self.num_comments = data["num_comments"] as UInt
+        self.over_18 = data["over_18"] as Bool
+        self.score = data["score"] as Int
+        self.subreddit = data["subreddit"] as String
+        self.subreddit_id = data["subreddit_id"] as String
+        self.thumbnail = data["thumbnail"] as String
+        self.title = data["title"] as String
+        self.url = data["url"] as String
     }
 }
--- Sandwich/ViewController.swift
+++ Sandwich/ViewController.swift
@@ -10,25 +10,26 @@ import UIKit
 
 class ViewController: UIViewController, UITextFieldDelegate, UITableViewDelegate, UITableViewDataSource {
     var tableView: UITableView!
-    var postData = [NSDictionary]()
+    var posts = [RedditPost]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Table view stuff
         let topBarHeight: CGFloat = 20
         self.tableView = UITableView(frame: CGRectMake(0, topBarHeight, self.view.bounds.width, self.view.bounds.height - topBarHeight), style: UITableViewStyle.Plain)
-        self.tableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: "myCell")
+        self.tableView.registerClass(DetailTableViewCell.self, forCellReuseIdentifier: "myCell")
+        self.tableView.rowHeight = UITableViewAutomaticDimension
         
         self.tableView.dataSource = self
         self.tableView.delegate = self
 
         self.view.addSubview(self.tableView)
         
-        Reddit.getPosts("youtubehaiku", done: self.setPostData)
+        Reddit.getPosts("youtubehaiku", done: self.setPosts)
     }
     
-    func setPostData(data: [NSDictionary]) {
-        self.postData = data
+    func setPosts(data: [RedditPost]) {
+        self.posts = data
         self.tableView.reloadData()
     }
 
@@ -39,20 +40,29 @@ class ViewController: UIViewController, UITextFieldDelegate, UITableViewDelegate
 
     // Table view data source delegate
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return postData.count
+        return self.posts.count
     }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let newCell: UITableViewCell = tableView.dequeueReusableCellWithIdentifier("myCell", forIndexPath: indexPath) as UITableViewCell
-        let post = self.postData[indexPath.row]
-        newCell.textLabel?.text = post["title"] as? String
+        let post = self.posts[indexPath.row]
         
-        // TODO: why doesn't this work
-        if let ups = post["ups"] as? Int {
-            newCell.detailTextLabel?.text = String(ups)
-        }
+        newCell.textLabel?.text = post.title
+        newCell.detailTextLabel?.text = "+" + String(post.score)
 
         return newCell
     }
+}
+
+class DetailTableViewCell : UITableViewCell {
+    
+    override init(style: UITableViewCellStyle, reuseIdentifier: String!) {
+        super.init(style: UITableViewCellStyle.Subtitle, reuseIdentifier: reuseIdentifier)
+    }
+
+    required init(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
 }
 
