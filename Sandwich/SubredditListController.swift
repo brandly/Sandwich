2719fea7b4565d72fee05a52ed3eecb01992bb71
brandly <brandly13@gmail.com>
2014-09-28 22:22:37 -0400

    create SubredditController

---
+++ Sandwich/SubredditListController.swift
@@ -0,0 +1,9 @@
+//
+//  SubredditListController.swift
+//  Sandwich
+//
+//  Created by Matthew Brandly on 9/28/14.
+//
+//
+
+import Foundation
---
+++ Sandwich/autoresize.swift
@@ -0,0 +1,9 @@
+//
+//  autoresize.swift
+//  Sandwich
+//
+//  Created by Matthew Brandly on 9/28/14.
+//
+//
+
+import Foundation
